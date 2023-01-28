ebfbc1ae078b08d7ae65570887bb6f2c175cf17d
Andrea Manzi <manzolo@libero.it>
2023-01-28 12:15:21 +0100

    first commit

---
+++ zsh.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo "installing zsh..."
+sudo apt -qqy install zsh > /dev/null
+sudo apt -qqy install powerline fonts-powerline > /dev/null
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+git -C ~/.oh-my-zsh/custom/plugins clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git
+git -C ~/.oh-my-zsh/custom/plugins clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git
+git -C ~/.oh-my-zsh/custom/themes clone --depth=1 https://github.com/romkatv/powerlevel10k.git
+
+cat >~/.zshrc <<\END
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+ZSH=~/.oh-my-zsh
+DISABLE_AUTO_UPDATE=true
+DISABLE_MAGIC_FUNCTIONS=true
+ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true
+
+ZSH_THEME="powerlevel10k/powerlevel10k"
+plugins=(
+  git
+  git-flow
+  zsh-syntax-highlighting
+  zsh-autosuggestions
+  history
+  sudo
+  )
+
+source ~/.oh-my-zsh/oh-my-zsh.sh
+
+END
+
+sudo chsh -s $(which zsh) $(whoami)
